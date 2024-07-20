499ecc5eb95ae202cd636ce079ac8bbc9f81e3e2
JoabCoelh0 <josejoab003@gmail.com>
2024-07-20 11:16:24 -0300

    atualizaçôes das aulas

--- aula23/index.js
+++ aula23/index.js
@@ -23,6 +23,10 @@
 
 //console.log(vaiExecutar && falaOi())
 
+const a = 0
+const b = null
+const c = 'false'
+const d = false
+const e = NaN
 
-console.log(0 || false || null || 'Luiz Otavio' || true)
-
+console.log(a || b || c || d || e)
